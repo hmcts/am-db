759ee995d41311950b90b907f7244bbb759c5ca2
Nitish Singla <nitish.singla@hmcts.net>
2019-06-04 17:28:26 +0100

    initial commit for the AM-DB project.

--- docker/am-db/docker-entrypoint-initdb.d/01-init-db.sh
+++ docker/am-db/docker-entrypoint-initdb.d/01-init-db.sh
@@ -2,6 +2,11 @@
 
 set -e
 
+if [ -z "$AM_DB_USERNAME" ] || [ -z "$AM_DB_PASSWORD" ]; then
+  echo "ERROR: Missing environment variable. Set value for both 'AM_DB_USERNAME' and 'AM_DB_PASSWORD'."
+  exit 1
+fi
+
 # Create roles and databases
 psql -v ON_ERROR_STOP=1 --username postgres --set USERNAME=$AM_DB_USERNAME --set PASSWORD=$AM_DB_PASSWORD <<-EOSQL
   CREATE USER :USERNAME WITH PASSWORD ':PASSWORD';
