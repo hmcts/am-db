4878feca04c7487bcc5443fa88d6b28e5654e770
Nitish Singla <nitish.singla@hmcts.net>
2019-06-07 10:54:36 +0100

    Enabled the Gradle build on project.

---
+++ src/main/java/AccessManagementDatabase.java
@@ -0,0 +1,11 @@
+package uk.gov.hmcts.am;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccessManagementDatabase {
+    public static void main(String[] args) {
+        SpringApplication.run(AccessManagementDatabase.class, args);
+    }
+}
